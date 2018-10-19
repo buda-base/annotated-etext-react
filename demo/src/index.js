b4b5f5fddb89d649fb09a323dda87bb8c7780273
berger-n <quai.ledrurollin@gmail.com>
2018-10-19 17:38:35 +0200

    removed serviceWorker

--- demo/src/index.js
+++ demo/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import AppContainer from './containers/AppContainer';
-import * as serviceWorker from './serviceWorker';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './state/reducers';
@@ -44,10 +43,5 @@ ReactDOM.render(
   <Provider store={store}>
     <AppContainer />
   </Provider>, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
 
 store.dispatch(initiateApp("bdr:UTDEMO_01"))
