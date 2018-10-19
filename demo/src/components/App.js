104051ca8894e8b997ca46f910c9ceb50bea4e8d
berger-n <quai.ledrurollin@gmail.com>
2018-10-19 16:06:33 +0200

    fixed InfiniteScroll

--- demo/src/components/App.js
+++ demo/src/components/App.js
@@ -6,6 +6,7 @@ import './App.css';
 type Props = {
    IRI?:string,
    chunks? : [],
+   next?:number,
    onGetChunks : (iri:string,n:number) => void
 }
 
@@ -22,13 +23,16 @@ class App extends Component<Props,State> {
    }
 
    render() {
+
+      console.log("props",this.props)
+
       let chunks = [{"value":"Hello World"}]
       if(this.props.chunks) chunks = this.props.chunks.map(e => ({"value":e.chunkContents["@value"]}))
 
       return (
          <div className="App">
-            <InfiniteScroll hasMore={!this.props.loaded} pageStart={0}
-               loadMore={(e) => { if(chunks.length !== this.props.next) this.props.onGetChunks(this.props.IRI,chunks.length) } } >
+            <InfiniteScroll hasMore={this.props.IRI && chunks.length !== this.props.next } pageStart={0}
+                 loadMore={(e) => { this.props.onGetChunks(this.props.IRI,chunks.length) } } >
                   <AnnotatedEtextContainer chunks={chunks} />
             </InfiniteScroll>
          </div>
