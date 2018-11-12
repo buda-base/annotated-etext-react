eac42cec4e2a630a75a246b52ae349c0c0542b31
berger-n <quai.ledrurollin@gmail.com>
2018-11-12 17:28:03 +0100

    added Etext chunks mocking in tests

--- src/AnnotatedEtext.test.js
+++ src/AnnotatedEtext.test.js
@@ -6,6 +6,10 @@ import AnnotatedEtext from './AnnotatedEtext';
 import AnnotatedEtextContainer from './AnnotatedEtextContainer';
 import { createMockStore } from 'redux-test-utils';
 import Adapter from 'enzyme-adapter-react-16'
+import CollectionService from './CollectionService';
+import type {CollectionInfo, URL} from './AnnotationTypes';
+
+import {nock_data} from './mockdata';
 
 Enzyme.configure({ adapter: new Adapter() })
 
@@ -17,6 +21,7 @@ const shallowWithStore = (component, store) => {
 };
 
 describe('AnnotatedEtext tests', () => {
+
    it('creates default empty AnnotatedEtext', async() => {
       const testState = {
          test: {}
@@ -26,4 +31,16 @@ describe('AnnotatedEtext tests', () => {
       expect(typeof component).toBe('object')
       console.log(component.debug())
    });
+
+   it('testing etext mocking', async () => {
+
+      nock_data("http://purl.bdrc.io", "/graph/Chunks", {I_LIM: 10, R_RES: "bdr:UTDEMO_01", I_SEQ: 1 })
+
+      const data:Response = await fetch("http://purl.bdrc.io/graph/Chunks?I_LIM=10&R_RES=bdr:UTDEMO_01&I_SEQ=1")
+      const chunks:Object = await data.json()
+
+      console.log(JSON.stringify(chunks,null,3))
+   })
+
+
 });
--- src/CollectionService.test.js
+++ src/CollectionService.test.js
@@ -6,11 +6,11 @@ import type {CollectionInfo, URL} from './AnnotationTypes';
 import {nock_data} from './mockdata';
 
 describe('testing collection service', () => {
- 
+
   it('calls service and return collections for a resource', async () => {
 
     nock_data("http://api.bdrc.io", "/annotations/collectionSearch", {res: "bdr:UTDEMO_01"});
- 
+
     const res: string = "bdr:UTDEMO_01";
     const serviceUrl: string = "http://api.bdrc.io/annotations/collectionSearch";
 
@@ -19,5 +19,7 @@ describe('testing collection service', () => {
     const collections: Array<CollectionInfo> = await service.getAnnotationCollectionsServices(res);
 
     expect(collections.length).toEqual(1);
+
+    console.log(JSON.stringify(collections,null,3))
   })
 })
